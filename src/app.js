2553ed9f19a60b3a7736b2ceab1cdbfc20062480
CabsT <133862761+CabsT@users.noreply.github.com>
2023-10-20 18:45:05 +0200

    Added styling to my weather info section and positioning of the elements in div class="col-4"in

--- src/app.js
+++ src/app.js
@@ -1,26 +1,3 @@
-let now = new Date();
-let daysOfWeek = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = daysOfWeek[now.getDay()];
-let hour = now.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
-}
-let min = now.getMinutes();
-if (min < 10) {
-  min = `0${min}`;
-}
-
-let h2Date = document.querySelector("#date");
-h2Date.innerHTML = `${day} ${hour}:${min}`;
-
 let search = document.querySelector("button");
 search.addEventListener("click", searchForCity);
 
@@ -41,6 +18,46 @@ function showTemp(response) {
   let temp = Math.round(response.data.temperature.current);
   let tempCel = document.querySelector(".temperature");
   tempCel.innerHTML = temp;
+  let dateTime = document.querySelector("#date");
+  dateTime.innerHTML = ShowdateTime(response.data.time);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
+}
+
+function ShowdateTime(timeStamp) {
+  let now = new Date(timeStamp * 1000);
+  let daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = daysOfWeek[now.getDay()];
+  console.log(day);
+  let hour = now.getHours();
+  console.log(hour);
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let min = now.getMinutes();
+  if (min < 10) {
+    min = `0${min}`;
+  }
+
+  return `Last updated: 
+  ${day} ${hour}:${min}`;
 }
 
 let current = document.querySelector(".current");
@@ -52,7 +69,6 @@ function showCurrentCityTemp(event) {
 }
 
 function showPosition(position) {
-  console.log(position);
   let lat = position.coords.latitude;
   let long = position.coords.longitude;
   let apiKey = "f0t6f37fo7eacab2cf93452fbe48b35c";
@@ -67,4 +83,18 @@ function showCurrentTemp(response) {
   tempC.innerHTML = temp;
   let currentCity = document.querySelector("h1");
   currentCity.innerHTML = city;
+  let dateTime = document.querySelector("#date");
+  dateTime.innerHTML = ShowdateTime(response.data.time);
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.condition.description;
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.temperature.humidity;
 }
